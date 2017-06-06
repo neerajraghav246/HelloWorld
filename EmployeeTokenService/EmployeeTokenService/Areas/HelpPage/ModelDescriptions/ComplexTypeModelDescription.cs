d66943aefd1a3ba27dd08665cb7318c84cf5d7be
neerajraghav246 <neeraj.raghav246@gmail.com>
2017-06-06 20:49:00 +0530

    First commit

---
+++ EmployeeTokenService/EmployeeTokenService/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -0,0 +1,14 @@
+using System.Collections.ObjectModel;
+
+namespace EmployeeTokenService.Areas.HelpPage.ModelDescriptions
+{
+    public class ComplexTypeModelDescription : ModelDescription
+    {
+        public ComplexTypeModelDescription()
+        {
+            Properties = new Collection<ParameterDescription>();
+        }
+
+        public Collection<ParameterDescription> Properties { get; private set; }
+    }
+}
---
+++ EmployeeTokenService/EmployeeTokenService/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDAL;
+
+namespace EmployeeTokenService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using(var _context=new EmployeeDBEntities())
+            {
+                return _context.Employees.ToList();
+            }
+        }
+    }
+}
